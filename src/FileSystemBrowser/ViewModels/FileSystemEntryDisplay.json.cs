0ebc2b4ebcbae5c2809bfc0d7877c95689b13d8f
Per Samuelsson <per@starcounter.com>
2015-12-17 15:39:00 +0100

    Improved viewing and browsing of file system entries.

--- src/FileSystemBrowser/ViewModels/FileSystemEntryDisplay.json.cs
+++ src/FileSystemBrowser/ViewModels/FileSystemEntryDisplay.json.cs
@@ -3,17 +3,23 @@ using FileSystemBrowser.Model;
 
 namespace FileSystemBrowser.ViewModels {
 
-    partial class FileSystemEntryDisplay : Partial, IBound<FileSystemEntry> {
+    partial class FileSystemEntryDisplay : Page, IBound<FileSystemEntry> {
         public string UriBinding {
             get {
                 var t = this.Data;
                 return string.Format("/filesystembrowser/entries/{0}", t.GetObjectID());
             }
         }
+
+        protected override void OnData() {
+            base.OnData();
+            IsDirectory = Data is DirectoryEntry;
+            SnapshotUri = string.Format("/filesystembrowser/snapshots/{0}", Data.Root.GetObjectID());
+        }
     }
 
     [FileSystemEntryDisplay_json.ParentEntry]
-    partial class FileSystemEntryParentDisplay : Partial, IBound<FileSystemEntry> {
+    partial class FileSystemEntryParentDisplay : Page, IBound<FileSystemEntry> {
         public string UriBinding {
             get {
                 var t = this.Parent.Data as FileSystemEntry;
